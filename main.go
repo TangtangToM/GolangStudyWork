19fe67392364c6adfb03340faed42e75bad33fb5
唐浩 <tangtang@192.168.0.110>
2021-10-31 21:09:50 +0800

    main.go

--- main.go
+++ main.go
@@ -1,1 +1,44 @@
-package GolangStudyWork
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/pkg/errors"
+	"os"
+)
+
+package main
+
+import (
+"database/sql"
+"fmt"
+"github.com/pkg/error"
+"os"
+)
+
+//遇到sql.ErrNoRows,Wrap error抛给上层
+func GetData() error {
+	return errors.Wrap(sql.ErrNoRows,"sql: no rows in result!")
+}
+
+
+func QurrySQL() error {
+
+	return errors.WithMessage(GetData(),"Qurry:call failed")
+}
+
+
+//上层处理
+func main() {
+
+
+	err := QurrySQL()
+	if err != nil {
+		fmt.Printf("original error, %T %v\n", errors.Cause(err),errors.Cause(err))
+		fmt.Printf("stack trace:\n%+v\n",err)
+		os.Exit(1)
+	}
+
+}
+
+
